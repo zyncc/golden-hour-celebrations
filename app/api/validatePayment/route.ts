3145d1ef57afff30b30b9d3aaa5ca4d6d72dbce8
zync <90741920+Zyncc1402@users.noreply.github.com>
2024-12-03 21:16:30 +0530

    fixed api validate bug?

--- app/api/validatePayment/route.ts
+++ app/api/validatePayment/route.ts
@@ -1,7 +1,5 @@
 import { SendReceipt } from "@/actions/emails";
 import prisma from "@/lib/prisma";
-import axios from "axios";
-import { SHA256 } from "crypto-js";
 import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
@@ -10,45 +8,20 @@ export async function POST(req: Request) {
   const decodedResponse = Buffer.from(response, "base64").toString("utf-8");
   const parsedResponse = JSON.parse(decodedResponse);
   console.log("Decoded Response:", parsedResponse);
-
   if (parsedResponse.success) {
-    const mtrID = parsedResponse.data.merchantTransactionId;
     try {
-      const checksum =
-        SHA256(
-          `/pg/v1/status/${process.env.MERCHANT_ID}/${mtrID}` +
-            process.env.SALT_KEY
-        ) +
-        "###" +
-        process.env.SALT_INDEX;
-      const options = {
-        method: "get",
-        url: `${
-          process.env.NODE_ENV == "development"
-            ? "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/status"
-            : "https://api.phonepe.com/apis/hermes/v1/status"
-        }/${process.env.MERCHANT_ID}/${mtrID}`,
-        headers: {
-          accept: "application/json",
-          "Content-Type": "application/json",
-          "X-VERIFY": checksum,
-          "X-MERCHANT-ID": process.env.MERCHANT_ID,
+      const mtrID = parsedResponse.data.merchantTransactionId;
+      revalidatePath("/book");
+      await prisma.reservations.update({
+        where: {
+          merchantTransactionID: mtrID,
         },
-      };
-      const response = await axios.request(options);
-      if (response.data.code == "PAYMENT_SUCCESS") {
-        revalidatePath("/booking");
-        await prisma.reservations.update({
-          where: {
-            merchantTransactionID: mtrID,
-          },
-          data: {
-            paymentStatus: true,
-            paymentTransactionID: response.data.data.transactionId,
-          },
-        });
-        SendReceipt(mtrID);
-      }
+        data: {
+          paymentStatus: true,
+          paymentTransactionID: response.data.data.transactionId,
+        },
+      });
+      SendReceipt(mtrID);
     } catch (error) {
       console.log(error);
     }
