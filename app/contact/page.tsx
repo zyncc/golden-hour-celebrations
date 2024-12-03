bdf342f931cbab4d1d2c337a08406268c8f8e4a0
zync <90741920+Zyncc1402@users.noreply.github.com>
2024-12-03 11:41:35 +0530

    mail change

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,9 +6,9 @@ export default function Page() {
     <div className="mt-[100px] container mx-auto">
       <h1 className="text-3xl font-semibold">Contact details :</h1>
       <h1 className="mt-4 font-medium">
-        Email: <Link href={"mailto:"}>goldenhourcelebrations@gmail.com</Link>
+        Email: goldenhourcelebrationsblr@gmail.com
       </h1>
-      <h1 className="font-medium">Phone: 9739204918</h1>
+      <h1 className="font-medium">Phone: +91 9739204918</h1>
     </div>
   );
 }
