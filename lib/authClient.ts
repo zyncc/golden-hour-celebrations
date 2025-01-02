a3ce0c1731801051ae9d8206cfd62655aee9896d
zync <90741920+Zyncc1402@users.noreply.github.com>
2025-01-02 22:43:46 +0530

    /

--- lib/authClient.ts
+++ lib/authClient.ts
@@ -2,6 +2,9 @@ import { createAuthClient } from "better-auth/react";
 import { adminClient, phoneNumberClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL,
+  baseURL:
+    process.env.NODE_ENV == "development"
+      ? "http://localhost:3000"
+      : "https://goldenhourcelebrations.in",
   plugins: [phoneNumberClient(), adminClient()],
 });
