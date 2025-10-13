0e4a3ed1318b5a1d100637f03e4464e23b5801c3
zyncc <chandankrishna288@gmail.com>
2025-10-13 14:14:38 +0530

    .

--- app/(admin)/dashboard/users/create/page.tsx
+++ app/(admin)/dashboard/users/create/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import CreateNewAdminForm from "./_components/client";
+import { auth } from "@/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth.api.getSession({
+    headers: headers(),
+  });
+  if (session?.user.role !== "admin") {
+    return redirect("/dashboard/signin");
+  }
   return (
     <div className="container mx-auto h-screen w-full flex items-center justify-center flex-col gap-y-5">
       <h1 className="text-xl font-semibold">Register New Admin</h1>
